0b3d295240fc35838472bd36fa4c853821a063af
Gomis Florian <florian.gomis@viacesi.fr>
2013-10-24 17:17:46 +0200

    Add of Httprequest to update Json before to load data from server. Correction in code since we change the type of urlMp3 and urlImage in trackInfo

--- OTPO-Player/downloadManager.m
+++ OTPO-Player/downloadManager.m
@@ -1,12 +1,16 @@
-//
-//  downloadManager.m
-//  Tableview Tutorial 3
-//
-//  Created by Gomis Florian on 10/21/13.
-//  Copyright (c) 2013 Gomis Florian. All rights reserved.
-//
+/**
+ * \brief Retrieve ImageData By using TrackInfo object which 
+ *  contain all information for one Track.
+ *
+ *
+ * \todo
+ * \args No Argument when you crete the object
+ * \return to homeViewController
+ */
 
 #import "downloadManager.h"
+
+
 @interface downloadManager()
 @property (strong, nonatomic) jsonRetriever *jsonRetriever;
 @property (strong, nonatomic) NSMutableArray *arrayOfTrack;
@@ -20,21 +24,22 @@
     self = [super init];
     if (self)
     {
-        _arrayOfTrack = nil;
+        NSURLRequest *scriptUpdateJson = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.otpo.fr/index.php/News/news_control/all_new"]];
+        NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:scriptUpdateJson delegate:self startImmediately:YES];
         
+        _arrayOfTrack = nil;
         _jsonRetriever = [[jsonRetriever alloc] initWithUrl:@"http://www.otpo.fr/Liste.json"];
         [self.jsonRetriever retrieveJson];
-        
         // We use NSNOtification Center in order to receive signal When one picture is finish to download
         // With this we will be able to reload the view and see change on the screen.
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(GetarrayOfTrack:)
                                                      name:@"jsonLoaded"
                                                    object:nil];
+        
     }
     return self;
 }
-
 
 - (void)GetarrayOfTrack:(NSNotification *) notification{
     self.arrayOfTrack = [self.jsonRetriever getJson];
@@ -58,7 +63,7 @@
     //Eachtime we receive Data, we add bytes to our NSMutableArray. For this we check the url use in the connection and the url save in the NSMutableArray.
     // When they match, we just need to add the bytes to the pre existant Bytes.
     for (int i = 0; i < [self.arrayOfTrack count]; i++) {
-        if ([[[connection originalRequest] URL] absoluteString] == [self.arrayOfTrack[i] urlPicture])
+        if ([[[connection originalRequest] URL] absoluteString] == [[self.arrayOfTrack[i] urlPicture] absoluteString])
         {
             [[self.arrayOfTrack[i] dataPicture] appendData:data];
         }
@@ -81,7 +86,7 @@
     // That we find in imageUrl.
 -(void)startDownload{
         for (int i = 0; i < [self.arrayOfTrack count]; i++){
-        NSURLRequest *theRequest = [NSURLRequest  requestWithURL:[NSURL URLWithString:[self.arrayOfTrack[i] urlPicture]]
+        NSURLRequest *theRequest = [NSURLRequest  requestWithURL:[self.arrayOfTrack[i] urlPicture]
                                                   cachePolicy:NSURLRequestUseProtocolCachePolicy
                                                   timeoutInterval:60.0];
         NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
@@ -90,8 +95,6 @@
         }
     }
 }
-
-
 
 -(NSMutableArray*) getMyData{
     return self.arrayOfTrack;
